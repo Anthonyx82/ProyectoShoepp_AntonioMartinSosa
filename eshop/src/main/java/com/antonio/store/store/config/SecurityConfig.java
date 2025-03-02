7ffa4a4477201e32697491170b09e2d316c2faf8
Anthonyx82 <antoniomartinmanzanares2004@gmail.com>
2025-03-02 17:31:20 +0100

    TIENDA DE ZAPATILLAS - SPRINGBOOT

--- eshop/src/main/java/com/antonio/store/store/config/SecurityConfig.java
+++ eshop/src/main/java/com/antonio/store/store/config/SecurityConfig.java
@@ -26,31 +26,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return SecurityUtility.passwordEncoder();
 	}
 
-	private static final String[] PUBLIC_MATCHERS = {
-			"/springboot/css/**", "/springboot/js/**", "/springboot/image/**", "/springboot/", "/springboot/new-user", "/springboot/login",
-			"/springboot/store", "/springboot/article-detail"
-	};
+	private static final String[] PUBLIC_MATCHERS = { "/css/**", "/js/**", "/image/**", "/", "/new-user", "/login",
+			"/store", "/article-detail" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers(PUBLIC_MATCHERS).permitAll()
-				.antMatchers("/springboot/article/**").hasRole("ADMIN")
-				.anyRequest().authenticated()
-				.and()
-				.formLogin()
-				.loginPage("/springboot/login")
-				.defaultSuccessUrl("/springboot/my-profile", true)
-				.failureUrl("/springboot/login?error")
-				.permitAll()
-				.and()
-				.logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/springboot/logout"))
-				.logoutSuccessUrl("/springboot/?logout")
-				.deleteCookies("remember-me")
-				.permitAll()
-				.and()
-				.rememberMe().key("aSecretKey");
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().antMatchers("/article/**").hasRole("ADMIN")
+				.anyRequest().authenticated();
+
+		http.csrf().disable().cors().disable().formLogin().failureUrl("/login?error").loginPage("/login").permitAll()
+				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/?logout")
+				.deleteCookies("remember-me").permitAll().and().rememberMe().key("aSecretKey");
 	}
 
 	@Autowired
